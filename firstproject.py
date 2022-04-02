cf4f5a74e42f36c8eec1f4d3c08884a57be17c54
paupedros <74618879+paupedros@users.noreply.github.com>
2022-04-02 17:57:26 +0200

    Add files via upload

---
+++ firstproject.py
@@ -0,0 +1,13 @@
+from turtle import pos, position
+
+
+def finalCalculus(l):
+    punts1 = 0
+    punts2 = 0
+    for equip in l:
+        for info in equip:
+            print((info) position: ())
+
+finalCalculus([["Manresa", 10, 1, 0, 10, 5], ["Barcelona", 3, 1, 1, 5, 5]])
+    
+        
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    
+    name = "paquetecalculos",
+    version = "1.0",
+    description = "Paquete de calculos basicos",
+    author = "Pau",
+    packages = ["calculos", "calculos.basicos"]
+    
+)
